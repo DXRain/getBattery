7e2f331d4ccd09bd341c0a84e44ea6947d69db4e
bigtree <bigtree@bigtreedeMBP.lan>
2017-05-31 04:49:55 -0700

    change

--- getBattery/getBattery/ViewController.m
+++ getBattery/getBattery/ViewController.m
@@ -7,8 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "IOPSKeys.h"
-#import "IOPowerSources.h"
+
 @interface ViewController ()
 
 @end
@@ -33,68 +32,6 @@
     [self.view addSubview:label];
 }
 
-- (void)getBatterySecond{
- 
-    /*
-     第二种方法获取电量
-     */
-    CGFloat batteryQuantity = [self getBatteryQuantity];
-    UILabel * label = [[UILabel alloc]initWithFrame:CGRectMake(20, 100, 300, 50)];
-    label.text = [NSString stringWithFormat:@"二:手机剩余电量:%.2f",batteryQuantity];
-    label.backgroundColor = [UIColor greenColor];
-    [self.view addSubview:label];
-}
-
--(double)getCurrentBatteryLevel
-{
-    
-    //Returns a blob of Power Source information in an opaque CFTypeRef.
-    CFTypeRef blob = IOPSCopyPowerSourcesInfo();
-    
-    //Returns a CFArray of Power Source handles, each of type CFTypeRef.
-    CFArrayRef sources = IOPSCopyPowerSourcesList(blob);
-    
-    CFDictionaryRef pSource = NULL;
-    const void *psValue;
-    
-    //Returns the number of values currently in an array.
-    int numOfSources = CFArrayGetCount(sources);
-    
-    //Error in CFArrayGetCount
-    if (numOfSources == 0)
-    {
-        NSLog(@"Error in CFArrayGetCount");
-        return -1.0f;
-    }
-    
-    //Calculating the remaining energy
-    for (int i = 0 ; i < numOfSources ; i++)
-    {
-        //Returns a CFDictionary with readable information about the specific power source.
-        pSource = IOPSGetPowerSourceDescription(blob, CFArrayGetValueAtIndex(sources, i));
-        if (!pSource)
-        {
-            NSLog(@"Error in IOPSGetPowerSourceDescription");
-            return -1.0f;
-        }
-        psValue = (CFStringRef)CFDictionaryGetValue(pSource, CFSTR(kIOPSNameKey));
-        
-        int curCapacity = 0;
-        int maxCapacity = 0;
-        double percent;
-        
-        psValue = CFDictionaryGetValue(pSource, CFSTR(kIOPSCurrentCapacityKey));
-        CFNumberGetValue((CFNumberRef)psValue, kCFNumberSInt32Type, &curCapacity);
-        
-        psValue = CFDictionaryGetValue(pSource, CFSTR(kIOPSMaxCapacityKey));
-        CFNumberGetValue((CFNumberRef)psValue, kCFNumberSInt32Type, &maxCapacity);
-        
-        percent = ((double)curCapacity/(double)maxCapacity * 100.0f);
-        
-        return percent;
-    }
-    return -1.0f;
-}
 
 -(CGFloat)getBatteryQuantity
 {
